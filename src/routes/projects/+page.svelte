2d609fdee354baa8b0eaa7587a79ee16bd7b7296
compass chip <itsjustchipoverhere@proton.me>
2025-09-17 22:07:54 +0700

    gh build

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Projects from "$lib/components/projects/projects.svelte";
+  import gar1 from "$lib/assets/gar1.jpg";
+  import gar2 from "$lib/assets/gar2.jpg";
+  import gar3 from "$lib/assets/gar3.jpg";
+
+
+  const myProjects = [
+    {
+      title: "Lution",
+      description: "A boostrapper for sober",
+      image: gar1,
+      link: "https://github.com/Wookhq/Lution"
+    },
+    {
+      title: "Ropon",
+      description: "A really simple and very cool™ roblox api helper lib for developersA really simple and very cool™ roblox api helper lib for developers",
+      image: gar2,
+      link: "https://github.com/Wookhq/ropon"
+    },
+    {
+      title: "Scratch2Json",
+      description: "A command-line tool for converting Scratch .sb3 projects into structured JSON and compiling structured JSON back into Scratch .sb3 format.",
+      image: gar3,
+      link: "https://github.com/whoschip/scratch2json"
+    }
+  ];
+</script>
+
+<Projects projects={myProjects} />
---
+++ src/routes/socials/+page.svelte
@@ -0,0 +1,5 @@
+<section class="flex flex-col items-center justify-center min-h-screen text-center space-y-6">
+  <h1 class="text-6xl font-bold bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 text-transparent bg-clip-text">
+    Working in progress
+  </h1>
+</section>
