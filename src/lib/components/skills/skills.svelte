f4e4b37c553e3a0bc3d126ca065b2c37d00981c8
compass chip <itsjustchipoverhere@proton.me>
2025-09-17 22:07:05 +0700

    skill also

---
+++ src/lib/components/skills/skills.svelte
@@ -0,0 +1,43 @@
+<section class="flex flex-col items-center justify-center min-h-screen space-y-8">
+  <h2 class="text-4xl font-bold">skillz</h2>
+
+  <div class="grid gap-6 md:grid-cols-2">
+    <!-- JS -->
+    <div class="card w-80 bg-base-100 shadow-sm">
+      <div class="card-body">
+        <h2 class="card-title">JavaScript</h2>
+        <progress class="progress w-full [&::-webkit-progress-value]:bg-yellow-400 [&::-moz-progress-bar]:bg-yellow-400"
+                  value="70"
+                  max="100">
+                  </progress>
+      </div>
+    </div>
+
+    <!-- SvelteKit -->
+    <div class="card w-80 bg-base-100 shadow-sm">
+      <div class="card-body">
+        <h2 class="card-title">SvelteKit</h2>
+        <progress class="progress w-full [&::-webkit-progress-value]:bg-red-400 [&::-moz-progress-bar]:bg-red-400"
+                  value="70"
+                  max="100">
+                  </progress>
+      </div>
+    </div>
+
+    <!-- Tailwind -->
+    <div class="card w-80 bg-base-100 shadow-sm">
+      <div class="card-body">
+        <h2 class="card-title">TailwindCSS</h2>
+        <progress class="progress progress-accent w-full" value="30" max="100"></progress>
+      </div>
+    </div>
+
+    <!-- Python -->
+    <div class="card w-80 bg-base-100 shadow-sm">
+      <div class="card-body">
+        <h2 class="card-title">Python</h2>
+        <progress class="progress progress-info w-full" value="80" max="100"></progress>
+      </div>
+    </div>
+  </div>
+</section>
